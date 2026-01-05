fec1b63b055fe72f7e36433fb7c46363a9c4a7bb
maahi <122282309+202201419@users.noreply.github.com>
2026-01-05 12:12:00 +0530

    Enhance BookRepository with additional storage methods

--- src/main/java/com/example/book_inventory/Repository/BookRepository.java
+++ src/main/java/com/example/book_inventory/Repository/BookRepository.java
@@ -3,16 +3,15 @@ package com.example.book_inventory.Repository;
 import com.example.book_inventory.Book;
 import org.springframework.stereotype.Repository;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 
 @Repository
 public class BookRepository {
 
     private final Map<Integer, Book> bookStorebyId = new HashMap<>();
     private final Map<String, Book> bookStorebyAuthorandTitle = new HashMap<>();
+    private final List books=new ArrayList();
+    private final Map<String,List<Book>> bookStoredbyAuthor = new HashMap<>();
 
     public boolean existsById(int bookId) {
         return bookStorebyId.containsKey(bookId);
@@ -21,6 +20,15 @@ public class BookRepository {
     public Book save(Book book) {
         bookStorebyId.put(book.getBookId(), book);
         bookStorebyAuthorandTitle.put(book.getAuthor()+"|"+book.getTitle(),book);
+        books.add(book);
+
+        List<Book> books = bookStoredbyAuthor.get(book.getAuthor().toLowerCase());
+        if (books == null) {
+            books = new ArrayList<>();
+            bookStoredbyAuthor.put(book.getAuthor().toLowerCase(), books);
+        }
+        books.add(book);
+
         return book;
     }
 
@@ -30,5 +38,19 @@ public class BookRepository {
 
     public Book findbyAuthorandTitle(String author, String title){
         return bookStorebyAuthorandTitle.get(author+"|"+title);
+    }
+
+    public List<Book> getAllBooks(){
+        return books;
+    }
+
+    public void deletebyid(Book book) {
+        books.remove(book);
+        bookStorebyAuthorandTitle.remove(book.getAuthor()+"|"+book.getTitle());
+        bookStorebyId.remove(book.getBookId());
+    }
+
+    public List<Book> getAllBooksbyAuthor(String author){
+        return bookStoredbyAuthor.get(author);
     }
 }
