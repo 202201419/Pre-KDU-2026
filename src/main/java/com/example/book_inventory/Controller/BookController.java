b13915e862a5dbac4ac26392a6048e1fbd1b37d7
maahi <122282309+202201419@users.noreply.github.com>
2026-01-05 12:11:42 +0530

    Update BookController.java

--- src/main/java/com/example/book_inventory/Controller/BookController.java
+++ src/main/java/com/example/book_inventory/Controller/BookController.java
@@ -6,6 +6,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/books")
 public class BookController {
@@ -16,10 +18,16 @@ public class BookController {
     }
 
     @PostMapping
-    public ResponseEntity<Book> addBook(@RequestBody Book book) {
-        Book savedBook = bookService.addBook(book);
-        return new ResponseEntity<>(savedBook, HttpStatus.CREATED);
+    public ResponseEntity<?> addBook(@RequestBody Book book) {
+        try {
+            Book savedBook = bookService.addBook(book);
+            return new ResponseEntity<>(savedBook, HttpStatus.CREATED);
+
+        } catch (IllegalArgumentException ex) {
+            return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+        }
     }
+
 
     @GetMapping("/{id}")
     public ResponseEntity<?> getBookById(@PathVariable int id) {
@@ -44,4 +52,40 @@ public class BookController {
                     HttpStatus.NOT_FOUND);
         }
     }
+
+    @GetMapping("/all")
+    public ResponseEntity<?> getAllbooks(){
+        try{
+            List<Book> books= bookService.getBooks();
+            return new ResponseEntity<>(books, HttpStatus.OK);
+        }
+        catch(IllegalArgumentException ex){
+            return new ResponseEntity<>(ex.getMessage(),HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping("/all/{author}")
+    public ResponseEntity<?> getAllBooksbyAuthor(@PathVariable String author){
+        try{
+            List<Book> booksbyAuthor= bookService.getBookbyAuthor(author);
+            return new ResponseEntity<>(booksbyAuthor,HttpStatus.OK);
+        }
+        catch(IllegalArgumentException ex){
+            return new ResponseEntity<>(ex.getMessage(),HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @DeleteMapping("/dlt/{id}")
+    public ResponseEntity<?> deletebook(@PathVariable int id){
+        try {
+            bookService.deletebook(bookService.getBookById(id));
+            List<Book> books= bookService.getBooks();
+            return new ResponseEntity<>(books, HttpStatus.NO_CONTENT);
+        }
+        catch(IllegalArgumentException ex){
+            return new ResponseEntity<>(ex.getMessage(),HttpStatus.NOT_FOUND);
+        }
+
+    }
 }
+
